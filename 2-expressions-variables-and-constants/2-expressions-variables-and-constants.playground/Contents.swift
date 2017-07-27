26c048255368024e739b5c299a771680cee5204b
Felipe Amaral <pulga.lipe@gmail.com>
2017-07-27 17:22:51 -0300

    Fixing stuff

---
+++ 2-expressions-variables-and-constants/2-expressions-variables-and-constants.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+print("Hello, Swift Apprentice reader!")
+
+// Simple operations
+2 + 6
+10 - 2
+2 * 4
+24 / 3
+
+// Decimal Numbers Operations
+22.0 / 7.0
+
+// Remainder Operation
+28 % 10
+
+// Shift Operator - Used to operate the number using Binary form of the number
+1 << 3
+32 >> 2
+
+// Order of Operations
+((8000 / (5 * 10)) - 32) >> (29 % 5)
+350 / 5 + 2
+350 / (5 + 2)
